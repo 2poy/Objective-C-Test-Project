59bd3e5e7a082529ab3f181486cb8388b8fa6eed
Egor Soloviev <Egor.Soloviev@acronis.com>
2020-03-24 17:22:32 +0300

    add: ObjC test file

---
+++ Objective C Test Project/Test.m
@@ -0,0 +1,9 @@
+//
+//  Test.m
+//  Objective C Test Project
+//
+//  Created by Egor Solovev on 24.03.2020.
+//  Copyright © 2020 Egor Solovev. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
