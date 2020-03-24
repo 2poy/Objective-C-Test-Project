d8943d03fda2a07a0189f2d88b6df6587a78d307
Egor Soloviev <Egor.Soloviev@acronis.com>
2020-03-24 17:18:18 +0300

    Initial Commit

---
+++ Objective C Test Project/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Objective C Test Project
+//
+//  Created by Egor Solovev on 24.03.2020.
+//  Copyright © 2020 Egor Solovev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
